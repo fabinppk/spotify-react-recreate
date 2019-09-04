ef2cfaefb6fccbd8e53d6ed70349acbf7390de0f
Fabiano <fabinppk@hotmail.com>
2019-09-04 17:07:37 -0300

    feat: json return

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'http://spotify-recreate.s3-website-us-east-1.amazonaws.com'
 });
 
 export default api;
