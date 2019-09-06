3b9c4bc2e689d38cf0133aad47f9f768e99ef221
Fabiano <fabinppk@hotmail.com>
2019-09-06 11:31:47 -0300

    feat: remove reactotron from deploy

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,15 +4,11 @@ import reducers from './ducks';
 import sagas from './sagas';
 
 const middlewares = [];
-const sagaMonitor =
-    process.env.NODE_ENV === 'development' ? console.tron.createSagaMonitor() : null;
-const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
-const createAppropriateStore =
-    process.env.NODE_ENV === 'development' ? console.tron.createStore : createStore;
+const sagaMiddleware = createSagaMiddleware();
 
 middlewares.push(sagaMiddleware);
 
-const store = createAppropriateStore(reducers, compose(applyMiddleware(...middlewares)));
+const store = createStore(reducers, compose(applyMiddleware(...middlewares)));
 
 sagaMiddleware.run(sagas);
 
