356911f148d6693c8ad753ff836941b027f4a709
Fabiano <fabinppk@hotmail.com>
2019-09-06 11:46:34 -0300

    clean: reactotron

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
-import './config/reactotron';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
